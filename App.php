bd81fc5d5a07fb959d112e749c333d1c1341dc92
brianokinyi <brianokinyi.bo@gmail.com>
2018-06-08 18:34:38 +0000

    Two step send custom message

--- App.php
+++ App.php
@@ -8,40 +8,44 @@
 
         public function app($details, $phone) {
             if (count($details)==1){
-                $ussd_text="CON \nYour full name?\nFormat: FirstName LastName";
+                
+                $ussd_text="CON \nReply with the message you want to send!";
                 ussd_proceed($ussd_text); 
-            } 
 
-
+            } else if (count($details)==1) {
             
-            // Include Africa's Talking gateway
-            require_once('resources/AfricasTalkingGateway.php');
-
-
-            // Specify your authentication credentials
-            require_once('env.php');
-
-            // Client phone number
-            $recipients = $phone;
-            
-            // Tell 'em what I am... haha
-            $message    = "I am a lumberjack. I sleep all night and work all day!";
-
-            // Create a new instance of the bad ass gateway
-            $gateway    = new AfricasTalkingGateway($username, $apikey);
-           
-            try { 
-                // Thats it, hit send and we'll take care of the rest. 
-                $results = $gateway->sendMessage($recipients, $message);
-                            
-                foreach($results as $result) {
-                    // 
+                // Include Africa's Talking gateway
+                require_once('resources/AfricasTalkingGateway.php');
+    
+    
+                // Specify your authentication credentials
+                require_once('env.php');
+    
+                // Client phone number
+                $recipients = $phone;
+                
+                // Tell 'em what I am... haha
+                $message    = "I am a lumberjack. I sleep all night and work all day!";
+    
+                // Create a new instance of the bad ass gateway
+                $gateway    = new AfricasTalkingGateway($username, $apikey);
+               
+                try { 
+                    // Thats it, hit send and we'll take care of the rest. 
+                    $results = $gateway->sendMessage($recipients, $message);
+                                
+                    foreach($results as $result) {
+                        // 
+                    }
+                }catch ( AfricasTalkingGatewayException $e ) {
+                    echo "Encountered an error while sending: ".$e->getMessage();
                 }
-            }catch ( AfricasTalkingGatewayException $e ) {
-                echo "Encountered an error while sending: ".$e->getMessage();
+    
+                // Damn it bro!
+
             }
 
-            // Damn it bro!
+
         }
     }
 
