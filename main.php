40f49748dc2f60cd2df8c67b05842a5fe781f23e
brianokinyi <brianokinyi.bo@gmail.com>
2018-06-08 11:43:56 +0000

    First working instance

--- main.php
+++ main.php
@@ -2,13 +2,20 @@
     // Print the response as plain text so that the gateway can read it
     header('Content-type: text/plain');
 
+    // Try using POST with online NGROK and sandbox
+	$phonenumber = $_GET['MSISDN'];  
+    $sessionID = $_GET['sessionId'];  
+    $servicecode = $_GET['serviceCode'];  
+    $ussdString = $_GET['text'];
 
     // Get the parameters provided by Africa's Talking USSD gateway
-    $phonenumber = $_POST['phoneNumber'];
-    $sessionID = $_POST['sessionId'];  
-    $servicecode = $_POST['serviceCode'];  
-    $ussdString = $_POST['text'];
+    // $phonenumber = $_POST['phoneNumber'];
+    // $sessionID = $_POST['sessionId'];  
+    // $servicecode = $_POST['serviceCode'];  
+    // $ussdString = $_POST['text'];
 
+    // Required my class
+    require_once('App.php');
 
     //set default level to zero
 	$level = 0; 
@@ -27,7 +34,6 @@
 
     if ($level==0){  
         displaymenu();
-        ussd_proceed($ussd_text);    
     } 
     
     //  
@@ -36,7 +42,7 @@
             {  
             case 1: 			
                 $app = new App();
-                ussd_proceed($ussd_text);
+                $app->app($phonenumber);
                 break;   
             case 2:  //About
                 $about = new About();
@@ -49,7 +55,6 @@
         }//End switch  
     }  	
  
-
     function displaymenu(){  
 		$ussd_text="CON 1. Run app\n2. About";  
 		ussd_proceed($ussd_text);  
